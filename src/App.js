a6724f02f8e3fd38833747c99aea5629a6ddbfb7
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-02-05 19:09:35 -0500

    navigation working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './index.css';
+import { send } from 'emailjs-com';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import Page from './components/Page';
+
+
 
 function App() {
+  const [pages] = useState([
+    { name: "about" },
+    { name: "services" },
+    { name: "government" },
+    { name: "portfolio"},
+    { name: "contact" },
+]);
+
+const [currentPage, setCurrentPage] = useState(pages[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <Nav
+          pages={pages}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        ></Nav>
+      </Header>
+      <main>
+        <Page currentPage={currentPage}></Page>
+      </main>
+      <Footer></Footer>
     </div>
+    
   );
 }
 
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import backgroundCover from '../../assets/img/info-managment.jpg'
 
 function About(){
   const [pages] = useState([
@@ -10,9 +11,13 @@ function About(){
   const [currentPage] = useState(pages[0]);
 
   return (
-    <div className="masthead text-center-white d-flex">
-      <div>
-        
+    <div className="masthead text-center d-flex">
+      <div className="container my-auto">
+        <div className="col">
+          <img src={backgroundCover} alt="information management"></img>
+        <h1>help</h1>
+        <h1>Hello</h1>
+        </div>
       </div>
     </div>
   )
